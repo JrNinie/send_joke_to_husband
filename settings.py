57cbe15f0d698ea24e333d16fe852a410fd62689
jrninie <devwjr@gmail.com>
2021-05-28 16:31:50 +0200

    add func to generate joke from api

--- settings.py
+++ settings.py
@@ -1,11 +1,18 @@
 from dotenv import load_dotenv, find_dotenv
 import os
 
-load_dotenv(find_dotenv(),override=True)
+load_dotenv(find_dotenv(), override=True)
 
 # Sender
-SENDER_ACCOUNT=os.getenv("SENDER_ACCOUNT")
-SENDER_PASSWORD=os.getenv("SENDER_PASSWORD")
-SENDER_HOST=os.getenv("SENDER_HOST")
+SENDER_MAIL = os.getenv("SENDER_MAIL")
+SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
+SENDER_HOST = os.getenv("SENDER_HOST")
 
+# Receiver
+RECEIVER_MAIL = "xxx@gmail.com"
+SUBJECT = "Ta blague du jour, ça avance"
 
+# Joke api
+TOKEN = os.getenv("TOKEN")
+RANDOM_JOKE_URL = "https://www.blagues-api.fr/api/random"
+DISALLOWED_JOKE_TYPES = ["limit","dark", "blondes"]
